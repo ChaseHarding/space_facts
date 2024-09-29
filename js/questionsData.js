31c4383f77af4774a5f31352867c26efa32fe4f3
ChaseHarding <chaseharding965@gmail.com>
2024-09-28 17:46:28 -0700

    styled homepage buttons, began js for questions and answers data

---
+++ js/questionsData.js
@@ -0,0 +1,5 @@
+export const spaceFacts = [
+    { question: 'What is the largest planet in our solar system?', answer: 'Jupiter' },
+    { question: 'How many moons does Mars have?', answer: 'Two'},
+    { question: 'What is the closest planet to the sun?', answer: 'Mercury'}
+]
